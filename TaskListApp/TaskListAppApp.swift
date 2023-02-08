fe91c23dc633db7bdb960aaa4fcba5e8cc1616d0
beardmikle <ankmo@ya.ru>
2023-02-08 19:11:26 +0300

    Initial Commit

---
+++ TaskListApp/TaskListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListAppApp.swift
+//  TaskListApp
+//
+//  Created by beardmikle on 08.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
